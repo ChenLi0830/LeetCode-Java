d117459c6924d6a43fe4edf5ae23c5baed8ae31f
Chen Li <yichen.li0830@gmail.com>
2016-11-26 20:15:22 -0800

    344. Reverse String

---
+++ src/LeetCode344.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Chen on 2016-11-26.
+ */
+public class LeetCode344 {
+    public String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
